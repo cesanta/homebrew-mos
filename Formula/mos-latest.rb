bd2b1d66d34df1e324a970d4788db98529c79af3
rojer <rojer@rojer.me>
2019-01-30 21:20:27 +0000

    Cache govendor's cache between builds
    govendor clones ~850M of repos, cache them to speed up builds.

--- Formula/mos-latest.rb
+++ Formula/mos-latest.rb
@@ -16,21 +16,20 @@ class MosLatest < Formula
   end
   # update_hb end
 
+  depends_on "libftdi"
+  depends_on "libusb"
+  depends_on "libusb-compat"
   depends_on "go" => :build
   depends_on "govendor" => :build
-  depends_on "libftdi" => :build
-  depends_on "libusb" => :build
-  depends_on "libusb-compat" => :build
   depends_on "make" => :build
   depends_on "pkg-config" => :build
   depends_on "python" => :build
+  depends_on "rsync" => :build
 
   conflicts_with "mos", :because => "Use mos or mos-latest, not both"
 
   def install
     ENV["GOPATH"] = buildpath
-    ENV["GOOS"] = "darwin"
-    ENV["GOARCH"] = "amd64"
     ENV["CGO_ENABLED"] = "1"
     ENV["PATH"] += ":#{ENV["GOPATH"]}/bin"
 
@@ -45,7 +44,15 @@ class MosLatest < Formula
       File.open(path/"mos/pkg.version", "w") { |file| file.write(version) }
       File.open(path/"mos/pkg.build_id", "w") { |file| file.write(build_id) }
 
+      # GoVendor pulls a lot of packages, it makes sense to cache them between builds.
+      cachedir = "/tmp/.mos-gocache/"
+      if(File.directory?(cachedir))
+        system "rsync", "-a", "--delete", cachedir, buildpath+".cache/"
+      else
+        ohai "Note: Go package cache does not exist, next step may take a long time"
+      end
       system "govendor", "sync"
+      system "rsync", "-a", "--delete", buildpath+".cache/", cachedir
       system "make", "generate"
       system "go", "build", "-o", bin/"mos"
       prefix.install_metafiles
