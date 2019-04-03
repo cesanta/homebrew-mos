3e18a0ae43f1b7c2ea13a3919499345c08082672
mos <mos@builder.local>
2019-04-03 21:06:21 +0100

    update_hb: mos-latest 201904031955

--- Formula/mos-latest.rb
+++ Formula/mos-latest.rb
@@ -4,15 +4,14 @@ class MosLatest < Formula
   # update_hb begin
   desc "Mongoose OS command-line tool (latest)"
   homepage "https://mongoose-os.com/"
-  url "https://github.com/cesanta/mos-tool/archive/ea01910c2e849862f400b42c4f2c6d97566804c8.tar.gz"
-  sha256 "c863b1e3aa3a069f982d9c8aa45f7e5f8cf1b8d5c2caafc66c91a67aed27ca38"
-  version "201903291233"
+  url "https://github.com/cesanta/mos-tool/archive/d59624116e3a80aad5a0b6bce175e3f6a8f585fb.tar.gz"
+  sha256 "c922b29cfd1f07570046c028052b4e34d9a4e6b10cdb150585afe253c9e38bbc"
+  version "201904031955"
   head ""
 
   bottle do
     root_url "https://mongoose-os.com/downloads/homebrew/bottles-mos-latest"
     cellar :any
-    sha256 "afaa35bb0b1d8a4833029eb58aad0d854cda20aaf6aaa9b4462ac9ca7c11f33e" => :mojave
   end
   # update_hb end
 
