ba1d887a414bf37cf3b8a1d37bc920cb27cfda15
mos <mos@builder.local>
2019-07-30 15:37:12 +0100

    update_hb: mos-latest 201907301422

--- Formula/mos-latest.rb
+++ Formula/mos-latest.rb
@@ -12,6 +12,7 @@ class MosLatest < Formula
   bottle do
     root_url "https://mongoose-os.com/downloads/homebrew/bottles-mos-latest"
     cellar :any
+    sha256 "fdf914f50c59f29ec50344f1b629baddf32002c5100201968698a542e9f675d9" => :mojave
   end
   # update_hb end
 
