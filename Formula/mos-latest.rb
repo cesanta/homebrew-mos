628875fab968ab20f6e8af96674c2a82d2ea44df
Deomid Ryabkov <rojer9@fb.com>
2020-11-08 01:12:25 +0000

    update_hb: mos-latest 202011072248

--- Formula/mos-latest.rb
+++ Formula/mos-latest.rb
@@ -4,15 +4,14 @@ class MosLatest < Formula
   # update_hb begin
   desc "Mongoose OS command-line tool (latest)"
   homepage "https://mongoose-os.com/"
-  url "https://github.com/mongoose-os/mos/archive/7d543c623dcc1987bede975488c3d41837253b05.tar.gz"
-  sha256 "d8883c2d63ad1456baf32dca5041cec1d52b567947ae4bf17ea6b751eab65473"
-  version "202008211943"
+  url "https://github.com/mongoose-os/mos/archive/fca85d5c47e8c96ce5a8ec5f58af9437fe908482.tar.gz"
+  sha256 "835cc606df2be4c37dc0459395562d3af3944b6f98f4ff22061d866bb286802f"
+  version "202011072248"
   head ""
 
   bottle do
     root_url "https://mongoose-os.com/downloads/homebrew/bottles-mos-latest"
     cellar :any
-    sha256 "137badfd22d704a70d73925de2d6876d6b5b4c797fc9acdfda7f7cedc9e91e56" => :catalina # 202008211943
   end
   # update_hb end
 
